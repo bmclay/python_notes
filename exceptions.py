3aa94f0057f675c9a75b4f49e1d481c6fc74000c
bmclay <93384439+bmclay@users.noreply.github.com>
2021-11-03 15:23:51 -0400

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,7 @@
-# Errors detected during execution are called exceptions and are not unconditionally fatal
-# You can raise an exception with raise
+"""
+Errors detected during execution are called exceptions and are not unconditionally fatal
+You can raise an exception with raise
+"""
 raise Exception('Bad')
 # Other possible uses of raise include the FileExistsError
 raise FileExistsError('The file already exists')
