edc65049dbf3a167ec16c2e3f10bda54b2ff6f49
bmclay <93384439+bmclay@users.noreply.github.com>
2021-11-03 15:25:33 -0400

    Update fstrings.py

--- fstrings.py
+++ fstrings.py
@@ -1,6 +1,8 @@
-# F strings are newer in python. Not a part of older versions
-# A cool way to manipulate strings
-# Allows you to embed an expression in curly brackets in a variable
+"""
+F strings are newer in python. Not a part of older versions
+A cool way to manipulate strings
+Allows you to embed an expression in curly brackets in a variable
+"""
 var = 100
 x = f'hello {6 + 8} {var}'
 print(x)
